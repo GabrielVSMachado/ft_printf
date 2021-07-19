41ed0fd05ebe6e6246053ca9060a1d4e4ddc071e
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-19 14:36:44 -0300

    feat: add function to check flags

--- ft_printf.h
+++ ft_printf.h
@@ -1,7 +1,7 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-#define TRUE 1
-#define FALSE 0
+# define TRUE 1
+# define FALSE 0
 
 # include <stdarg.h>
 # include "./libft/libft.h"
@@ -11,12 +11,13 @@ typedef struct s_conversion
 	char	conversion;
 	char	*str;
 	size_t	len;
-	int	hashtag;
+	int		hashtag;
 }				t_conv;
 
 int		ft_printf(const char *format, ...);
 void	get_conversion(t_conv *param, va_list ap);
 void	get_flags(t_conv *param);
+void	check_flags_conversion(t_conv *param, char *format);
 char	*ft_conversion_p(void *param);
 char	*ft_conversion_s(char *param);
 char	*ft_conversion_c(int c, size_t *len);
