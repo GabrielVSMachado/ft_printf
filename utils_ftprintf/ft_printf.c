3aeaddee37de377229527f04967c621bad8d991e
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-19 13:21:19 -0300

    feat: function to print no NULL strings

--- utils_ftprintf/ft_printf.c
+++ utils_ftprintf/ft_printf.c
@@ -6,11 +6,12 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/18 17:09:13 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/07/18 23:49:35 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/07/19 13:10:25 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
+static void	ft_putstr2_fd(char *str, int fd);
 
 int	ft_printf(const char *format, ...)
 {
@@ -27,7 +28,7 @@ int	ft_printf(const char *format, ...)
 		{
 			conv_handler.conversion = *(++format_p);
 			get_conversion(&conv_handler, ap);
-			ft_putstr_fd(conv_handler.str, 1);
+			ft_putstr2_fd(conv_handler.str, 1);
 			free(conv_handler.str);
 			format_p++;
 		}
@@ -39,4 +40,10 @@ int	ft_printf(const char *format, ...)
 		}
 	}
 	return (va_end(ap), conv_handler.len);
+}
+
+static void	ft_putstr2_fd(char *str, int fd)
+{
+	while (str && *str)
+		ft_putchar_fd(*str++, fd);
 }
