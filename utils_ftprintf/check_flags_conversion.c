6759446492c6606fefb67155ce7490d24dbe65ae
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-19 16:32:36 -0300

    feat:function to initialize values in struct

--- utils_ftprintf/check_flags_conversion.c
+++ utils_ftprintf/check_flags_conversion.c
@@ -6,15 +6,17 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 13:55:03 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/07/19 14:27:31 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/07/19 16:31:34 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 static int	isconversion(int c);
+static void	initializer_struct(t_conv *param);
 
 void	check_flags_conversion(t_conv *param, char *format_p)
 {
+	initializer_struct(param);
 	while (!isconversion(*format_p))
 	{
 		if (*format_p == '#')
@@ -29,4 +31,11 @@ static int	isconversion(int c)
 	if (ft_strchr("cspiudxX%", c))
 		return (1);
 	return (0);
+}
+
+static void	initializer_struct(t_conv *param)
+{
+	param->conversion = FALSE;
+	param->hashtag = FALSE;
+	param->str = NULL;
 }
