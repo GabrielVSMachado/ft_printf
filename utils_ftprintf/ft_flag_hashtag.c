4a1d0a125561c3ab1dd7ff544702e67bfe913407
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-19 13:45:17 -0300

    feat: add function to handle with hashtag flag

---
+++ utils_ftprintf/ft_flag_hashtag.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_flag_hashtag.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/19 13:25:49 by gvitor-s          #+#    #+#             */
+/*   Updated: 2021/07/19 13:29:55 by gvitor-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_printf.h"
+
+void	ft_flag_hashtag(t_conv *param)
+{
+	char	*tmp;
+
+	tmp = param->str;
+	if (param->conversion == 'x')
+		param->str = ft_strjoin("0x", param->str);
+	else
+		param->str = ft_strjoin("0X", param->str);
+	free(tmp);
+}
