72168456d72a8014acc2919038ac238e07528179
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-18 23:47:24 -0300

    feat: ft_conversion receive size_t pointer

--- ft_printf.h
+++ ft_printf.h
@@ -15,7 +15,7 @@ int		ft_printf(const char *format, ...);
 void	get_conversion(t_conv *param, va_list ap);
 char	*ft_conversion_p(void *param);
 char	*ft_conversion_s(char *param);
-char	*ft_conversion_c(int c);
+char	*ft_conversion_c(int c, size_t *len);
 char	*ft_conversion_d_i(int d);
 char	*ft_conversion_u(unsigned int u);
 char	*ft_conversion_x(unsigned int x);
