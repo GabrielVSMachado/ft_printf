e9200129839579eb3cc736aefc04c842ab5b5d17
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-19 19:44:10 -0300

    refactory: now works with param # 0

--- utils_ftprintf/get_flags.c
+++ utils_ftprintf/get_flags.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 13:37:00 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/07/19 13:42:32 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/07/19 19:41:24 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	get_flags(t_conv *param)
 	char	c;
 
 	c = param->conversion;
-	if (param->hashtag == TRUE && (c == 'x' || c == 'X'))
+	if (param->hashtag == TRUE && (c == 'x' || c == 'X')
+		&& ft_strncmp(param->str, "0", 1))
 		ft_flag_hashtag(param);
 }
