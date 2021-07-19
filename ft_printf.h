080174e3255f053c4d7ec6cfbf4ab209bae71e4d
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-19 13:47:26 -0300

    feat: add functions to handle with flags

--- ft_printf.h
+++ ft_printf.h
@@ -1,5 +1,7 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+#define TRUE 1
+#define FALSE 0
 
 # include <stdarg.h>
 # include "./libft/libft.h"
@@ -9,10 +11,12 @@ typedef struct s_conversion
 	char	conversion;
 	char	*str;
 	size_t	len;
+	int	hashtag;
 }				t_conv;
 
 int		ft_printf(const char *format, ...);
 void	get_conversion(t_conv *param, va_list ap);
+void	get_flags(t_conv *param);
 char	*ft_conversion_p(void *param);
 char	*ft_conversion_s(char *param);
 char	*ft_conversion_c(int c, size_t *len);
@@ -20,5 +24,6 @@ char	*ft_conversion_d_i(int d);
 char	*ft_conversion_u(unsigned int u);
 char	*ft_conversion_x(unsigned int x);
 char	*ft_conversion_X(unsigned int x);
+void	ft_flag_hashtag(t_conv *param);
 
 #endif
