96924f9a04d5a24d2ed66a56d5158703d538d46f
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-19 20:54:51 -0300

    refacory: check flag space

--- utils_ftprintf/check_flags_conversion.c
+++ utils_ftprintf/check_flags_conversion.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 13:55:03 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/07/19 17:07:31 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/07/19 20:26:58 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ void	check_flags_conversion(t_conv *param, char **format_p)
 	{
 		if (**format_p == '#')
 			param->hashtag = TRUE;
+		else if (**format_p == ' ')
+			param->space = TRUE;
 		(*format_p)++;
 	}
 	param->conversion = **format_p;
@@ -38,5 +40,6 @@ static void	initializer_struct(t_conv *param)
 {
 	param->conversion = FALSE;
 	param->hashtag = FALSE;
+	param->space = FALSE;
 	param->str = NULL;
 }
