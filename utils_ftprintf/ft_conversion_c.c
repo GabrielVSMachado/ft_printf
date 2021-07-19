d139a787a89debca9fff75b2dbe212eebad6ddf3
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-19 13:20:37 -0300

    refactor: return NULL when the param is \0

--- utils_ftprintf/ft_conversion_c.c
+++ utils_ftprintf/ft_conversion_c.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/15 00:27:49 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/07/18 23:44:30 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/07/19 13:11:15 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ char	*ft_conversion_c(int c, size_t	*len)
 {
 	char	*str;
 
+	str = NULL;
 	if (c == 0)
 	{
 		write(1, "\0", 1);
 		*len += 1;
-		return (ft_strdup(""));
 	}
-	str = ft_calloc(2, sizeof(char));
-	if (!str)
-		return (NULL);
-	str[0] = (char)c;
+	else
+	{
+		str = ft_calloc(2, sizeof(char));
+		if (!str)
+			return (NULL);
+		str[0] = (char)c;
+	}
 	return (str);
 }
