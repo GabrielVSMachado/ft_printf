bb78b9dd339bbcb0f323da5a612302e408effccb
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-18 23:48:05 -0300

    feat: print \0 when passed as param

--- utils_ftprintf/ft_conversion_c.c
+++ utils_ftprintf/ft_conversion_c.c
@@ -6,19 +6,25 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/15 00:27:49 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/07/15 00:52:00 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/07/18 23:44:30 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-char	*ft_conversion_c(int c)
+char	*ft_conversion_c(int c, size_t	*len)
 {
 	char	*str;
 
-	str = ft_calloc(2, 1);
+	if (c == 0)
+	{
+		write(1, "\0", 1);
+		*len += 1;
+		return (ft_strdup(""));
+	}
+	str = ft_calloc(2, sizeof(char));
 	if (!str)
 		return (NULL);
-	str[0] = c;
+	str[0] = (char)c;
 	return (str);
 }
