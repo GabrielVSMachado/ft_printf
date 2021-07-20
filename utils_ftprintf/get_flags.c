7697eca09890212c6243d9c423eac1dd2cd4eaef
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-19 23:37:19 -0300

    refactory: now get flag plus

--- utils_ftprintf/get_flags.c
+++ utils_ftprintf/get_flags.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 13:37:00 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/07/19 21:05:54 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/07/19 23:29:55 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@ void	get_flags(t_conv *param)
 		&& ft_strncmp(param->str, "0", 1))
 		ft_flag_hashtag(param);
 	if (param->space == TRUE && (c == 'i' || c == 'd')
-		&& ft_atoi(param->str) >= 0)
+		&& ft_atoi(param->str) >= 0 && param->plus == FALSE)
 		ft_flag_space(param);
+	if (param->plus == TRUE && (c == 'd' || c == 'i')
+		&& ft_atoi(param->str) > 0)
+		ft_flag_plus(param);
 }
