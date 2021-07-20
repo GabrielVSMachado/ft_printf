01373d77278eff747d539d589cec68b90075f53a
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-19 23:36:54 -0300

    refactory: now check for plus

--- utils_ftprintf/check_flags_conversion.c
+++ utils_ftprintf/check_flags_conversion.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 13:55:03 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/07/19 20:26:58 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/07/19 23:25:32 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,8 @@ void	check_flags_conversion(t_conv *param, char **format_p)
 			param->hashtag = TRUE;
 		else if (**format_p == ' ')
 			param->space = TRUE;
+		else if (**format_p == '+')
+			param->plus = TRUE;
 		(*format_p)++;
 	}
 	param->conversion = **format_p;
@@ -41,5 +43,6 @@ static void	initializer_struct(t_conv *param)
 	param->conversion = FALSE;
 	param->hashtag = FALSE;
 	param->space = FALSE;
+	param->plus = FALSE;
 	param->str = NULL;
 }
