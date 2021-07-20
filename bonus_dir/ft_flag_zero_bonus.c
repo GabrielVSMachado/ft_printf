2dab04088a25173e24840b30fb69b7c0e08c06d9
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-20 18:25:38 -0300

    refactory: now len is int

--- bonus_dir/ft_flag_zero_bonus.c
+++ bonus_dir/ft_flag_zero_bonus.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/20 13:48:16 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/07/20 18:15:49 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/07/20 18:24:49 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_flag_zero(t_conv *param)
 {
 	char	c;
-	size_t	len;
+	int		len;
 	char	*tmp;
 
 	c = param->conversion;
