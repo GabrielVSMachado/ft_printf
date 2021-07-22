16f1d23e56e13bc7abf77f76d9d35c7f096fa9fd
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-22 18:58:59 -0300

    refactory: now receive for params

--- bonus_dir/ft_flag_zero_bonus.c
+++ bonus_dir/ft_flag_zero_bonus.c
@@ -6,36 +6,36 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/20 13:48:16 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/07/21 01:56:20 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/07/22 18:58:10 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-void	ft_flag_zero(t_conv *param)
+void	ft_flag_zero(char conversion, int width, size_t *len_str, char **str)
 {
 	char	c;
 	int		len;
 	char	*tmp;
 
-	c = param->conversion;
-	len = param->width - param->len_str;
+	c = conversion;
+	len = width - *len_str;
 	if (len > 0)
 	{
-		if ((c == 'i' || c == 'd') && param->str[0] == '-')
+		if ((c == 'i' || c == 'd') && *str[0] == '-')
 		{
-			tmp = ft_strdup(&(param->str[1]));
-			free(param->str);
-			param->str = tmp;
-			tmp = ft_memset(ft_calloc(len + param->len_str + 1, 1),
+			tmp = ft_strdup(&(*str[1]));
+			free(*str);
+			*str = tmp;
+			tmp = ft_memset(ft_calloc(len + *len_str + 1, 1),
 					'0', len + 1);
 			tmp[0] = '-';
 		}
 		else
-			tmp = ft_memset(ft_calloc(len + param->len_str + 1, 1), '0', len);
-		ft_strlcat(tmp, param->str, len + param->len_str + 1);
-		free(param->str);
-		param->str = tmp;
-		param->len_str += len;
+			tmp = ft_memset(ft_calloc(len + *len_str + 1, 1), '0', len);
+		ft_strlcat(tmp, *str, len + *len_str + 1);
+		free(*str);
+		*str = tmp;
+		*len_str += len;
 	}
 }
