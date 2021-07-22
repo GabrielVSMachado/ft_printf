b7914afa0cb86a6821163156d63fa2ffc984bc70
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-22 19:25:29 -0300

    refactory: treated precision

--- bonus_dir/get_flags_bonus.c
+++ bonus_dir/get_flags_bonus.c
@@ -6,7 +6,7 @@
 /*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 13:37:00 by gvitor-s          #+#    #+#             */
-/*   Updated: 2021/07/22 19:13:39 by gvitor-s         ###   ########.fr       */
+/*   Updated: 2021/07/22 19:22:59 by gvitor-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	get_flags(t_conv *param)
 	char	c;
 
 	c = param->conversion;
+	if (param->precision >= 0 && (c != 'c' || c != '%'))
+		ft_precision(param);
 	if (param->hashtag == TRUE && (c == 'x' || c == 'X')
 		&& ft_strncmp(param->str, "0", 1))
 		ft_flag_hashtag(param);
@@ -28,8 +30,7 @@ void	get_flags(t_conv *param)
 		ft_flag_plus(param);
 	if ((c != 's' && c != 'c' && c != '%') && param->zero == TRUE
 		&& param->minus == FALSE)
-		ft_flag_zero(param->conversion, param->width,
-			&(param->len_str), &(param->str));
+		ft_flag_zero(c, param->width, &(param->len_str), &(param->str));
 	if (param->minus == TRUE)
 		ft_flag_minus(param);
 }
