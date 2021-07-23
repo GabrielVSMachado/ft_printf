c2e0e45b31f0a2c55e6c579915d087317c78ae46
GabrielVSM <gabrielvitorsales@gmail.com>
2021-07-23 00:44:09 -0300

    Add: function width_bonus

---
+++ bonus_dir/ft_width_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_width_bonus.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gvitor-s <gvitor-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/23 00:31:57 by gvitor-s          #+#    #+#             */
+/*   Updated: 2021/07/23 00:43:13 by gvitor-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_printf.h"
+
+void	ft_width(t_conv *param)
+{
+	char	*tmp;
+	int		len;
+
+	len = param->width - param->len_str;
+	if (len > 0)
+	{
+		tmp = ft_memset(ft_calloc(len + param->len_str + 1, 1), ' ', len);
+		ft_strlcat(tmp, param->str, len + param->len_str + 1);
+		free(param->str);
+		param->str = tmp;
+	}
+}
